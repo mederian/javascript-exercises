9db099fa7302f76a02d036c6c6dbe137469794ed
mederian <ole.snekkermo@gmail.com>
2023-07-17 18:22:08 +0200

    Completed exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, repeats) {
+    
+    let repeatedString = ''
+    if(repeats < 0) return "ERROR"
+    for (let i = 0; i < repeats; i++){
+        repeatedString += stringToRepeat
+    }
 
+    return repeatedString;
 };
 
 // Do not edit below this line
