332d217a74b652d085b3bd82b9f48189ae71feff
mederian <ole.snekkermo@gmail.com>
2023-07-17 18:57:38 +0200

    Exercise 4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, valueToRemove) {
+    
+    //Loop to handle the unkown number of arguments
+    for(let i = 0; i < arguments.length - 1; i++){
+            // Find position in string where to remove array; 
+        let v = 0;
+        for(val of arr){
+            if (val === arguments[i+1]) break;
+            v++;
+        }
+        arr.splice(v,1)
+        
 
+    }
+    console.log(arr);
+    return arr;
 };
 
 // Do not edit below this line
