e6ff986ab2b504c0ecfebb19e9a81cde61a04233
mederian <ole.snekkermo@gmail.com>
2023-07-19 17:54:54 +0200

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    //if divisable by 4
+    // but not 100
+    //unless 400
+    if(year % 4 === 0){
+        if(year % 100 === 0){
+            if(year % 400 === 0){
+                return true;
+            }
+            return false;    
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
