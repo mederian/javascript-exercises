44dfc178f32a5b88e8860a6d943b31002eb49789
mederian <ole.snekkermo@gmail.com>
2023-07-19 17:48:05 +0200

    Exercise 05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(numA,numB) {
+    if(typeof numA !== 'number' || typeof numB !== 'number') return "ERROR";
+    if(numA < 0 || numB < 0) return "ERROR";
+    let start = numA;
+    let end = numB;
+    if(numA > numB){
+        start = numB;
+        end = numA;
+    } 
+    
+    let i = start;
+    let sum = 0;
+    while(i <= end){
+        sum += i++;
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
