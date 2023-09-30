d2ea96f528124c70d8985cda0eb43eb926149630
mederian <ole.snekkermo@gmail.com>
2023-09-30 16:39:08 +0200

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
 
+
+//Fibonacci
+// f0= 0 and f1= 1, f2 =1, f3 = 2, f4 = 3, f5 = 5, f6 = 8, f7 = 13
+const fibonacci = function(f) {
+    if(f < 0) return "OOPS";
+    let a = 0;
+    let b = 1;
+    let res = 0;
+    for(i = 0; i<=(f-2); i++){
+        let b2 = a+b;
+        a = b;
+        b = b2;
+    }
+    return b;
 };
 
 // Do not edit below this line
