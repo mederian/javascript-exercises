500a6a3afdec233616225865015ba9dac6f92cd1
mederian <ole.snekkermo@gmail.com>
2023-09-30 09:41:32 +0200

    on task 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,21 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(m) {
+  let sum = 1;
+  for(let i = 0; i < arguments.length; i++){
+    sum *= arguments[i];
+  }
+  return sum;
 
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+  // Easy: Math.pow(num,exp);
+	if(exp === 0) return 0;
+  if(exp === 1) return num;
+  let res = 1;
+  for(let i = 0; i < exp; i++){
+    res *=num;
+  }
+ 
+  return res;
 };
 
 const factorial = function() {
