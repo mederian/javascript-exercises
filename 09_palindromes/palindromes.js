5a0e80b3c01d69f45e941e1eb18583e35e0ed3de
mederian <ole.snekkermo@gmail.com>
2023-09-30 16:24:29 +0200

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    //Remove comma, dot, and spaces
+    //create a reverse, and check if same
+    text = text.replaceAll(' ', '');
+    text = text.replaceAll(',', '');
+    text = text.replaceAll('!', '');
+    text = text.replaceAll('?', '');
+    text = text.replaceAll('.', '');
+    text = text.toLowerCase();
 
+    let textArr = text.split('');
+    let reverseTextArr = textArr.reverse();
+    let reversedText = reverseTextArr.join('');
+    console.log(`RESULT: ${text} <-> ${reversedText}`);
+    if(text === reversedText) return true;
+    return false;
+    
 };
 
 // Do not edit below this line
