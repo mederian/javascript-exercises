4eb488ab837bc6bef2658fa40f2a31e9e59aabca
Ole Marius Snekkermo <ole.snekkermo@gmail.com>
2023-09-30 23:11:41 +0200

    cleaned some code

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,19 +1,10 @@
 const findTheOldest = function(people) {
-    // for every people in people. subtract death from birthyear. But if has no death year, used current year
-
     people.sort(ageSort);
-
-
-    console.log(people);
-
     return people.at(-1);
 
-    function ageSort(yearsA, yearsB){
-        
+    function ageSort(yearsA, yearsB){   
         const yearADeath = typeof yearsA['yearOfDeath'] !== 'undefined' ? yearsA['yearOfDeath'] : (new Date().getFullYear());
         const yearBDeath = typeof yearsB['yearOfDeath'] !== 'undefined' ? yearsB['yearOfDeath'] : (new Date().getFullYear());
-        console.log(yearsA, yearsB);
-        console.log(yearADeath, yearBDeath);
 
         if(yearADeath - yearsA['yearOfBirth'] < yearBDeath - yearsB['yearOfBirth']) return -1
         if(yearADeath - yearsA['yearOfBirth'] > yearBDeath - yearsB['yearOfBirth']) return 1
